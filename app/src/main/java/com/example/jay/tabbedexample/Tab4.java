59939b1a305bb1c80a8e69ed5b1abf8af682f936
jayashribaskaran <jayashribaskaran@gmail.com>
2018-02-26 08:34:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jay/tabbedexample/Tab4.java
@@ -0,0 +1,16 @@
+package com.example.jay.tabbedexample;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab4 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab4, container, false);
+        return rootView;
+    }
+}
